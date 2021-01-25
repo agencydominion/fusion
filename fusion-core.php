64dabc1c7976eb6c4f1e7a709937b1b00ab067c3
Jeff Chew <jeff.chew@agencydominion.com>
2021-01-24 23:43:30 -0400

    Move material icons to separately enqueued CSS file

--- fusion-core.php
+++ fusion-core.php
@@ -294,6 +294,7 @@ class FusionCore	{
 		wp_enqueue_script('images_loaded', plugin_dir_url( __FILE__ ) .'includes/utilities/imagesloaded/imagesloaded.pkgd.min.js', array('jquery'), '3.1.8', true);
 		//plugin
 		wp_enqueue_script( 'fsn_core', plugin_dir_url( __FILE__ ) . 'includes/js/fusion-core.js', array('jquery','modernizr','images_loaded'), '1.3.6', true );
+		wp_enqueue_style( 'material-icons', plugin_dir_url( __FILE__ ) . 'includes/css/material-icons.css', '', '1.5.4' );
 		wp_enqueue_style( 'fsn_core', plugin_dir_url( __FILE__ ) . 'includes/css/fusion-core.css', array('bootstrap'), '1.3.6' );
 
 		//setup front end script for use with AJAX
