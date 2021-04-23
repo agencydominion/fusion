ba9cba8ea4d4ee36ed519bfc317eb6d500eec691
Jeff Chew <jeff.chew@agencydominion.com>
2021-04-23 10:27:58 -0300

    Add button role attribute to modal links

--- includes/utilities/functions.php
+++ includes/utilities/functions.php
@@ -379,6 +379,7 @@ function fsn_get_button_anchor_attributes($button_object, $classes = false) {
 		}
 	} elseif (!empty($button_type) && $button_type == 'modal') {
 		$button_attributes .= ' data-toggle="modal"';
+		$button_attributes .= ' role="button"';
 	}
 	return apply_filters('fsn_button_anchor_attributes', $button_attributes, $button_object, $classes);
 }
