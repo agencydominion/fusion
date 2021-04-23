d2d33ae234b8bb60ed5ff750ee964cb015de9333
Jeff Chew <jeff.chew@agencydominion.com>
2021-04-23 10:51:17 -0300

    Update version number

--- fusion-core.php
+++ fusion-core.php
@@ -6,7 +6,7 @@
  * Plugin Name: Fusion : Plugin
  * Plugin URI: https://www.agencydominion.com/fusion/
  * Description: Create layouts for your page content in a rich visual editor.
- * Version: 1.5.5
+ * Version: 1.6.0
  * Author: Agency Dominion
  * Author URI: https://www.agencydominion.com
  * Text Domain: fusion
@@ -14,7 +14,7 @@
  * License: GPL2
  */
 
-define( 'FSN_VERSION', '1.5.5' );
+define( 'FSN_VERSION', '1.6.0' );
 
 /**
  * Fusion class.
