e8a8af72631b9a4d9ac5b0fc79126e984ec038c0
Jeff Chew <jeff.chew@agencydominion.com>
2024-09-03 14:29:33 -0300

    Code element : Prettify

--- includes/extensions/code.php
+++ includes/extensions/code.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package Fusion
  */
@@ -16,22 +17,23 @@
  */
 
 add_action('init', 'fsn_init_code', 12);
-function fsn_init_code() {
-	
-	if (function_exists('fsn_map')) {	
+function fsn_init_code()
+{
+
+	if (function_exists('fsn_map')) {
 		fsn_map(array(
 			'name' => __('Code', 'fusion'),
 			'shortcode_tag' => 'fsn_code',
 			'description' => __('Input HTML, CSS, or JavaScript code. Useful for developers looking to add in embeds and code that may normally be stripped or altered by WordPress Editor.', 'fusion'),
 			'icon' => 'code',
-			'params' => array(						
+			'params' => array(
 				array(
 					'type' => 'textarea',
 					'param_name' => 'code',
 					'label' => __('Code', 'fusion'),
 					'content_field' => true,
 					'encode_base64' => true
-				)			
+				)
 			)
 		));
 	}
@@ -41,10 +43,9 @@ function fsn_init_code() {
  * Output Shortcode
  */
 
-function fsn_code( $atts, $content ) {		
-	
-	return '<div class="fsn-code '. fsn_style_params_class($atts) .'">'. base64_decode( wp_strip_all_tags($content) ) .'</div>';
+function fsn_code($atts, $content)
+{
+
+	return '<div class="fsn-code ' . fsn_style_params_class($atts) . '">' . base64_decode(wp_strip_all_tags($content)) . '</div>';
 }
 add_shortcode('fsn_code', 'fsn_code');
-
-?>
