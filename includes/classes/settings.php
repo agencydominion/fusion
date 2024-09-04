1174a881b95ca055a05c85f94c7510aa182b79d7
Jeff Chew <jeff.chew@agencydominion.com>
2024-09-04 16:23:27 -0300

    Add escaping

--- includes/classes/settings.php
+++ includes/classes/settings.php
@@ -34,7 +34,7 @@ class FusionCoreSettings
 ?>
 		<div class="wrap">
 			<?php screen_icon(); ?>
-			<h2><?php _e('Fusion Settings', 'fusion'); ?></h2>
+			<h2><?php esc_html_e('Fusion Settings', 'fusion'); ?></h2>
 			<?php //settings_errors(); 
 			?>
 			<form action="options.php" method="post">
@@ -86,7 +86,7 @@ class FusionCoreSettings
 
 	public function fsn_output_general_settings()
 	{
-		echo '<p>' . __('Setup the Fusion plugin with your theme.', 'fusion') . '</p>';
+		echo '<p>' . esc_html__('Setup the Fusion plugin with your theme.', 'fusion') . '</p>';
 	}
 
 	public function fsn_post_types()
@@ -99,9 +99,9 @@ class FusionCoreSettings
 		// echo the fields
 		foreach ($post_types as $post_type) {
 			$post_type_object = get_post_type_object($post_type);
-			echo '<input name="fsn_options[fsn_post_types][]" type="checkbox" value="' . esc_attr($post_type) . '"' . (in_array($post_type, $fsn_post_types) ? ' checked="checked"' : '') . '>' . $post_type_object->labels->name . '<br>';
+			echo '<input name="fsn_options[fsn_post_types][]" type="checkbox" value="' . esc_attr($post_type) . '"' . (in_array($post_type, $fsn_post_types) ? ' checked="checked"' : '') . '>' . esc_html($post_type_object->labels->name) . '<br>';
 		}
-		echo '<p class="description">' . __('Choose the post types on which Fusion will be available.', 'fusion') . '</p>';
+		echo '<p class="description">' . esc_html__('Choose the post types on which Fusion will be available.', 'fusion') . '</p>';
 	}
 
 	public function fsn_bootstrap_enable_toggle()
@@ -111,7 +111,7 @@ class FusionCoreSettings
 		$fsn_bootstrap_enable = !empty($options['fsn_bootstrap_enable']) ? $options['fsn_bootstrap_enable'] : '';
 		// echo the field
 		echo '<input id="fsn_bootstrap_enable" name="fsn_options[fsn_bootstrap_enable]" type="checkbox" ' . checked($fsn_bootstrap_enable, 'on', false) . '>';
-		echo '<p class="description">' . __('Check this option if not using the Fusion Theme or if the active theme does not include Bootstrap.', 'fusion') . '</p>';
+		echo '<p class="description">' . esc_html__('Check this option if not using the Fusion Theme or if the active theme does not include Bootstrap.', 'fusion') . '</p>';
 	}
 
 	public function fsn_bootstrap_fluid_toggle()
@@ -121,7 +121,7 @@ class FusionCoreSettings
 		$fsn_bootstrap_fluid = !empty($options['fsn_bootstrap_fluid']) ? $options['fsn_bootstrap_fluid'] : '';
 		// echo the field
 		echo '<input id="fsn_bootstrap_fluid" name="fsn_options[fsn_bootstrap_fluid]" type="checkbox" ' . checked($fsn_bootstrap_fluid, 'on', false) . '>';
-		echo '<p class="description">' . __('Check this option to fit the grid into the active theme\'s content container (will be required for most themes).', 'fusion') . '</p>';
+		echo '<p class="description">' . esc_html__('Check this option to fit the grid into the active theme\'s content container (will be required for most themes).', 'fusion') . '</p>';
 	}
 }
 
